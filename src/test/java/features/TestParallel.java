804944bb8a2e7c16346283e2712136995e2bcc3e
AusraUrnezaite <ausra.urnezaite@gmail.com>
2023-03-07 16:29:42 +0200

    parallel test updated

--- src/test/java/features/TestParallel.java
+++ src/test/java/features/TestParallel.java
@@ -2,15 +2,14 @@ package features;
 
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TestParallel {
 
     @Test
     void testParallel() {
-        Results results = Runner.path("getapi").parallel(2);
+        Results results = Runner.path("src/test/java").parallel(10);
         assertEquals(0, results.getFailCount(), results.getErrorMessages());
     }
 }
